383b716e25e9665c74f1c3584ad692e993eee445
blue-tech-ae <programingbluetech@gmail.com>
2025-06-15 23:46:47 -0700

    refactor ui components

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,ts}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#2563eb'
+        }
+      }
+    },
   },
   plugins: [],
 }
